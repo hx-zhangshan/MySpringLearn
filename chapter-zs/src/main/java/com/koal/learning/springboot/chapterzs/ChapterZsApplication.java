0b16198c9f637ac3590b10cfe1ce700b1e9a8861
张山 <1367704944@qq.com>
2018-08-17 13:17:23 +0800

    fix

--- chapter-zs/src/main/java/com/koal/learning/springboot/chapterzs/ChapterZsApplication.java
+++ chapter-zs/src/main/java/com/koal/learning/springboot/chapterzs/ChapterZsApplication.java
@@ -2,7 +2,12 @@ package com.koal.learning.springboot.chapterzs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 
+ * @author pc
+ * 启动类
+ *
+ */
 @SpringBootApplication
 public class ChapterZsApplication {
 
