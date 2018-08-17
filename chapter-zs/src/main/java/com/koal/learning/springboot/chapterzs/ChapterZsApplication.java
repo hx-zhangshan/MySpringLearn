6ac6bd44add8d8557799c520ba8aaae46879f05a
张山 <1367704944@qq.com>
2018-08-17 18:02:05 +0800

    第六章学习

--- chapter-zs/src/main/java/com/koal/learning/springboot/chapterzs/ChapterZsApplication.java
+++ chapter-zs/src/main/java/com/koal/learning/springboot/chapterzs/ChapterZsApplication.java
@@ -2,13 +2,16 @@ package com.koal.learning.springboot.chapterzs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 /**
  * 
  * @author pc
  * 启动类
- *
+ * @PropertySource 获取自定义配置文件 因为 application中的 属性值获取 默认isso8859编码
+   *   不能识别中文 可以在自定义配置文件中 制定utf——8的编码格式
  */
 @SpringBootApplication
+@PropertySource(value="classpath:my.properties",encoding="utf-8")
 public class ChapterZsApplication {
 
 	public static void main(String[] args) {
--- chapter-zs/src/main/java/com/koal/learning/springboot/chapterzs/controller/DemoController.java
+++ chapter-zs/src/main/java/com/koal/learning/springboot/chapterzs/controller/DemoController.java
@@ -1,18 +1,30 @@
 package com.koal.learning.springboot.chapterzs.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.koal.learning.springboot.chapterzs.config.Config;
+
+import lombok.extern.log4j.Log4j;
 /**
  * 
  * @author zs 
  * @ResController=@controller+@requestBody 直接就返回Json
  */
+@Log4j
 @RestController
 public class DemoController {
 
+	
+	@Resource
+	private Config config;
 	@RequestMapping(value="/demo",method=RequestMethod.GET)
 	public String demo() {
-		return "hello SpringBoot!";
+		System.out.println(config.toString());
+		log.error(config.toString());
+		return "hello SpringBoot!"+config.getName();
 	}
 }
