747844443748f31ac2850ea80e8aadfeef92d86d
张山 <1367704944@qq.com>
2018-10-15 14:42:57 +0800

    提交

---
+++ chapter-zs-10/src/main/java/com/koal/learning/springboot/chapter/Hello.java
@@ -0,0 +1,11 @@
+package com.koal.learning.springboot.chapter;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		while(true) {
+			
+			System.out.println("java hello");
+		}
+	}
+}
