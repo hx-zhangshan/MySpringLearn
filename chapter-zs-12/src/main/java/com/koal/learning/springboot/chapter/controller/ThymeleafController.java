e919357fe55feb5cac03de569a3e726510be020b
张山 <1367704944@qq.com>
2018-10-18 17:52:33 +0800

    前端模板引擎 thymeleaf  配置文件 末尾不能有空格 ，尤其是 spring.thymeleaf.prefix=classpath:/templates/

--- chapter-zs-12/src/main/java/com/koal/learning/springboot/chapter/controller/ThymeleafController.java
+++ chapter-zs-12/src/main/java/com/koal/learning/springboot/chapter/controller/ThymeleafController.java
@@ -8,19 +8,19 @@ import org.springframework.web.servlet.ModelAndView;
 
 // 是要返回 页面 所以这里不能用 @Rescontroller
 @Controller
-@RequestMapping("/freemaker")
-public class FreeMakerController {
+@RequestMapping("/thymeleaf")
+public class ThymeleafController {
 
 	@GetMapping("/map")
 	public String index(String name,ModelMap map) {
 		map.addAttribute("name", name);
 		map.addAttribute("from", "zs.cn");
 		//
-		return "freemaker";
+		return "thymeleaf";
 	}
 	@GetMapping("/mv")
 	public ModelAndView index(String name,ModelAndView mv) {
-		mv.setViewName("freemaker");
+		mv.setViewName("thymeleaf");
 		mv.addObject("name", name);
 		mv.addObject("from", "zs.cn");
 		//模板名称。全路径应该是src/mian/resources/templates/freemaker.html
