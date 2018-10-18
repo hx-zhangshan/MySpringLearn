55771ccf82a6d936f2eb66aac3053cb6a156f22b
张山 <1367704944@qq.com>
2018-10-18 11:56:34 +0800

    配置前端模板引擎 freemaker

---
+++ chapter-zs-12/src/main/java/com/koal/learning/springboot/chapter/ChapterZs12Application.java
@@ -0,0 +1,17 @@
+package com.koal.learning.springboot.chapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 
+ * @author zhangS
+ *
+ * @date 2018年10月18日
+ */
+@SpringBootApplication
+public class ChapterZs12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChapterZs12Application.class, args);
+	}
+}
---
+++ chapter-zs-12/src/main/java/com/koal/learning/springboot/chapter/controller/FreeMakerController.java
@@ -0,0 +1,30 @@
+package com.koal.learning.springboot.chapter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+// 是要返回 页面 所以这里不能用 @Rescontroller
+@Controller
+@RequestMapping("/freemaker")
+public class FreeMakerController {
+
+	@GetMapping("/map")
+	public String index(String name,ModelMap map) {
+		map.addAttribute("name", name);
+		map.addAttribute("from", "zs.cn");
+		//
+		return "freemaker";
+	}
+	@GetMapping("/mv")
+	public ModelAndView index(String name,ModelAndView mv) {
+		mv.setViewName("freemaker");
+		mv.addObject("name", name);
+		mv.addObject("from", "zs.cn");
+		//模板名称。全路径应该是src/mian/resources/templates/freemaker.html
+		return mv;
+	}
+}
