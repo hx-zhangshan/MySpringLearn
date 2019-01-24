05bf18b4ac1af214712bcfa557c4f7fb93e8f026
张山 <1367704944@qq.com>
2019-01-24 17:13:35 +0800

    webSocket的简单应用

--- chapter-zs-13/src/main/java/com/koal/learning/springboot/chapter/ChapterZs13Application.java
+++ chapter-zs-13/src/main/java/com/koal/learning/springboot/chapter/ChapterZs13Application.java
@@ -2,11 +2,25 @@ package com.koal.learning.springboot.chapter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+/**
+ * webSocket 服务端简单应用。
+ * 
+ * @author zhangS
+ *
+ * @date 2019年1月24日
+ */
 @SpringBootApplication
+@EnableWebSocket
 public class ChapterZs13Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChapterZs13Application.class, args);
+	}
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter() {
+		return new ServerEndpointExporter();
 	}
 }
